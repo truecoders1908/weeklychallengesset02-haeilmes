2a8e50b9acd1cbd60b475a1e92c3da4f7067080f
Hunter <haeilmes@gmail.com>
2019-09-09 11:39:13 -0500

    Commit 2

--- weeklyChallenges02/ChallengesSet02.cs
+++ weeklyChallenges02/ChallengesSet02.cs
@@ -37,29 +37,71 @@ namespace weeklyChallenges02
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            int numberIsEven = number % 2;
+            if (numberIsEven == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            int numberIsOdd = num % 2;
+            if (numberIsOdd != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            else if(numbers.Count() == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return numbers.Min() + numbers.Max();
+            }
+
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if ( str1.Length <= str2.Length)
+            {
+                return str1.Length;
+            }
+            else
+            {
+                return str2.Length;
+            }
 
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if(numbers == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return numbers.Sum();
+            }
         }
 
         public int SumEvens(int[] numbers)
